52fa52ac376af9db57b597daf8bccfff96eda218
Prixma <28676630+PedroPrisxzma@users.noreply.github.com>
2020-11-01 19:13:06 -0300

    update

--- modules/state.py
+++ modules/state.py
@@ -2,9 +2,10 @@ import random
 from modules.action import Actions
 
 class State:
-    def __init__(self, posX: int, posY: int, char: str) -> State:
+    def __init__(self, posX: int, posY: int, char: str, steps: int):
         self.posX = posX
         self.posY = posY
+        self.steps = steps
         self.is_terminal = False if char == "." or char == "#" else True
         self.char = char
         self.reward = self.define_reward(char)
@@ -20,6 +21,7 @@ class State:
         else:
             return -10
     
+    #falta terminar
     def update_qvalue(learning_rate, discount_factor, next_state, action):
         max_next_state_qvalue = max(next_state.qvalues.values())
         next_state.reward
