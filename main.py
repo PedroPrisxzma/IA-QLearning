c3c67391a851de90304c8565dc42b0b687ee747a
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-11-01 18:16:12 -0300

    utility functions

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-import sys
-import modules
-
-algorithm = sys.argv[1]
-entry_file = sys.argv[2]
-
-graph = modules.graph.Graph()
-graph.generate_from_file(entry_file)
-
-possible_entry_points = [v for v in graph.possible_entries]
-
-ok = False
-
-if(ok):
-    PrintResult()
-else:
-    print("Algortihm not implemented")
+import random
+from modules.state import State
 
 
-def PrintResult():
-    return("Not Yet Implemented")
+def epsilon_greedy_policy(epsilon, state):
+    action_probabilities = [1, 1, 1, 1] * (epsilon / 4)
+    best_action = max(state.qvalues.values())
+    action_probabilities[best_action] += (1.0 - epsilon)
+    return action_probabilities
+
+
+def choose_start(state_map):
+    x = random.randrange(0, len(state_map))
+    y = random.randrange(0, len(state_map[x]))
+    if state_map[x][y].is_terminal:
+        choose_start(state_map)
+    else:
+        return state_map[x][y]
+
+
+def env_from_file(filepath):
+    """
+    Returns the state action grid from a file.
+    """
+
+    state_map = []
+
+    with open(filepath) as inputfile:
+        state_grid = [
+            [state for state in filter(lambda x: x in ["*", ".", "#", "$"], list(line))]
+            for line in inputfile
+        ]
+
+    for i in range(len(state_grid)):
+        state_line = []
+        for j in range(len(state_grid[i])):
+            state_line.append(State(i,j,state_grid[i][j]))
+        state_map.append(state_line)
+
+    return state_map
+
