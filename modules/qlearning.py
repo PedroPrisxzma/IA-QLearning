6197ebce6523c9dbb2d1d8f64945f96b4e7797c1
Prixma <28676630+PedroPrisxzma@users.noreply.github.com>
2020-11-02 19:00:31 -0300

    StateDic added

--- modules/qlearning.py
+++ modules/qlearning.py
@@ -8,6 +8,8 @@ def qLearning(
 
     agent = Agent(learning_rate, discount_factor, reward_map, max_steps)
 
+    stateDic = {}
+
     for _ in range(epochs):
 
         current_state = Agent.choose_start(reward_map, state_grid, max_steps)
@@ -18,7 +20,7 @@ def qLearning(
 
             action = random.choices([0, 1, 2, 3], weights=action_probabilities)
 
-            nextState, done = agent.take_action(current_state, action)
+            next_state, done = agent.take_action(current_state, action)
 
             # update
             current_state.update_qvalue(
@@ -27,6 +29,10 @@ def qLearning(
 
             if done:
                 break
+            
+            stateDic[(current_state.posX, current_state.posY, current_state.steps)] = current_state
+
+            current_state = next_state
 
         # ver como vai ser a saida no final
         return False
