7e3678be1cfd3c7d9b66e30485ebe6d5fb3fed47
Prixma <28676630+PedroPrisxzma@users.noreply.github.com>
2020-11-03 18:23:52 -0300

    bash done

--- qlearning.sh
+++ qlearning.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-ALGORITHM=$1
-INPUT=$2
+INPUT=$1
+ALPHA=$2
+EPSILON=$3
+LAMBDA=$4
+N=$5
 
-if [ "$ALGORITHM" = "-bfs" ] || [ "$ALGORITHM" = "-dfs" ] || 
-    [ "$ALGORITHM" = "-ids" ] || [ "$ALGORITHM" = "-astar" ] 
-then
-    #echo python3 main.py "${ALGORITHM//-}" "${INPUT//-}"
-    python3 main.py "${ALGORITHM#?}" "${INPUT#?}"
-else
-    echo Command must be ./busca.sh -algoritmo -entrada
-    echo Possible -algoritmo are -bfs, -dfs, -ids or -astar
-    echo -entrada must be a .txt indicating a maze, as specified
-fi
+python3 main.py ${INPUT} ${ALPHA} ${EPSILON} ${LAMBDA} ${N}
