6225df21705e4d8355e2bd395d8544fd953536da
Prixma <28676630+PedroPrisxzma@users.noreply.github.com>
2020-11-03 16:44:57 -0300

    graphing

--- modules/qlearning.py
+++ modules/qlearning.py
@@ -1,6 +1,6 @@
 import random
 from modules.agent import Agent
-
+import matplotlib.pyplot as plt
 
 def qLearning(
     learning_rate, discount_factor, epsilon, reward_map, state_grid, max_steps, epochs
@@ -9,10 +9,15 @@ def qLearning(
     agent = Agent(learning_rate, discount_factor, reward_map, state_grid, max_steps)
 
     stateDic = {}
+    all_epochs = []
+    epoch_rewards = []
 
-    for _ in range(epochs):
+    for e in range(epochs):
 
         current_state = Agent.choose_start(reward_map, state_grid, max_steps)
+
+        epoch_reward = 0
+        all_epochs.append(e)
 
         for _ in range(0, len(reward_map) * len(reward_map[0]) * 2):
 
@@ -27,9 +32,15 @@ def qLearning(
 
             stateDic[(current_state.posX, current_state.posY, current_state.steps)] = current_state
 
+            epoch_reward += reward
+
             if next_state.is_terminal:
+                epoch_rewards.append(epoch_reward)
                 break
 
             current_state = next_state
-
+        
+    print(all_epochs, epoch_rewards)
+    plt.plot(all_epochs[:len(epoch_rewards)], epoch_rewards)
+    plt.savefig('reward_cumulative.png')
     return stateDic
